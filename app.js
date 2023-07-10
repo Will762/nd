c6536518a750831f3d28f3fa4fa65d7d07b77b39
Will Hayman <will.hayman@aventri.com>
2023-07-10 22:08:39 +0100

    Tidy up

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 function solution(list){
 	const result = [];
-	let startRangeIndex = 0;
-	let stopRangeIndex = 1;
+	let limit = 1;
+	let rangeStart = rangeStop = 0;
 
 	function push() {
-		if ((stopRangeIndex - 1) - startRangeIndex > 2) {
-			result.push(`${list[startRangeIndex]}-${list[stopRangeIndex - 1]}`)
-		} else if (stopRangeIndex - 1 === startRangeIndex) {
-			result.push(`${list[startRangeIndex]}`);
+		if (rangeStop - rangeStart >= 2) {
+			result.push(`${list[rangeStart]}-${list[rangeStop]}`)
+		} else if (rangeStart === rangeStop) {
+			result.push(`${list[rangeStart]}`);
 		} else {
-			result.push(`${list[startRangeIndex]},${list[stopRangeIndex - 1]}`);
-		}
-	};
-
-	for (stopRangeIndex; stopRangeIndex < list.length; stopRangeIndex++) {
-		if (Math.abs(list[stopRangeIndex] - list[stopRangeIndex - 1]) > 1) {
-			push();
-			startRangeIndex = stopRangeIndex;
+			result.push(`${list[rangeStart]},${list[rangeStop]}`);
 		}
 	}
 
-	push();
-	return result;
-}
+	for (limit; limit < list.length; limit++) {
+		if (Math.abs(list[limit] - list[rangeStop]) > 1) {
+			push();
+			rangeStart = limit;
+		}
+		rangeStop = limit;
+	}
 
-// const x = solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
-const x = solution([0, 1, 2, 3, 4, 9, 10]);
-console.log(x);
+	push();
+	return result.join(',');
+}
