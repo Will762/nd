81a8ba63ae9c39be2c1e0ba19dcfa28b7586aad0
Will Hayman <will.hayman@aventri.com>
2023-07-12 20:19:10 +0100

    Simple Pig Latin https://www.codewars.com/kata/520b9d2ad5c005041100000f/

--- app.js
+++ app.js
@@ -1,55 +1,27 @@
-function getPINs(observed) {
-	const keypad = [
-		[1, 2, 3],
-		[4, 5, 6],
-		[7, 8, 9],
-		[null, 0, null]
-	];
-
-	const keypadCoords = {
-		1: [0, 0],
-		2: [0, 1],
-		3: [0, 2],
-		4: [1, 0],
-		5: [1, 1],
-		6: [1, 2],
-		7: [2, 0],
-		8: [2, 1],
-		9: [2, 2],
-		0: [3, 1],
-	};
-
-	const numbers = observed.split('');
-	const adjacentsGroups = numbers.map(number => {
-		const coords = keypadCoords[number];		
-		const results = [];
-		
-		coords.forEach((coord, i) => {
-			coords[i] = coord - 1;
-			if (keypad[coords[0]]?.[coords[1]]) {
-				results.push(keypad[coords[0]][coords[1]]);
-			}
-			coords[i] = coord + 1;
-			if (keypad[coords[0]]?.[coords[1]]) {
-				results.push(keypad[coords[0]][coords[1]]);
-			}
-			coords[i] = coord;
-		});
-		results.push(parseInt(number));
-		return results;
-	});
-
-	console.log(adjacentsGroups);
-
-	for (i = 0; i < adjacentsGroups.length; i++) {
-		for (j = 0; j < adjacentsGroups[i].length; j++) {
-
-			// console.log(adjacentsGroups[i][i] + '-' + adjacentsGroups[i][j]);
-		}
-	}
+function pigIt(str) {
+	if (!str.length) return str;
 	
-	// return results;
-}
-console.log(getPINs('103'));
+	const words = str.split(' ');
+	result = words.map(word => {
+		if (!word.match(/[A-Za-z0-9]+/)) return word;
+		const punctArr = [...word.matchAll(/[^\w\s]+/g)];
+		const alphaChars = [...word.match(/[A-Za-z0-9]+/)[0]];
 
-// console.log(solution([-4, -2, -1, 0, 1, 2, 3, 8, 10, 11, 12, 15, 16, 20, 21, 25]))
+		alphaChars.push(alphaChars[0] + 'ay');
+		alphaChars.shift();
+
+		if (punctArr.length) {
+			for (const punct of punctArr) {
+				if (punct['index']) {
+					alphaChars.push(punct[0]);
+				} else {
+					alphaChars.unshift(punct[0]);
+				}
+			}
+		}
+		return alphaChars.join('');
+	});
+	return result.join(' ');
+}
+
+console.log(pigIt('Hello world ! yo'));
