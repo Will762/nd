e23ad086a500339a671f6abe3afd67197fb29f44
Will Hayman <will.hayman@aventri.com>
2023-07-12 01:10:21 +0100

    The Observed PIN - WIP https://www.codewars.com/kata/5263c6999e0f40dee200059d/

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
-function solution(list){
-	const result = [];
-	let rangeStart = rangeStop = 0;
+function getPINs(observed) {
+	const keypad = [
+		[1, 2, 3],
+		[4, 5, 6],
+		[7, 8, 9],
+		[null, 0, null]
+	];
 
-	function push() {
-		if (rangeStop - rangeStart >= 2) {
-			result.push(`${list[rangeStart]}-${list[rangeStop]}`)
-		} else if (rangeStart === rangeStop) {
-			result.push(`${list[rangeStart]}`);
-		} else {
-			result.push(`${list[rangeStart]},${list[rangeStop]}`);
+	const keypadCoords = {
+		1: [0, 0],
+		2: [0, 1],
+		3: [0, 2],
+		4: [1, 0],
+		5: [1, 1],
+		6: [1, 2],
+		7: [2, 0],
+		8: [2, 1],
+		9: [2, 2],
+		0: [3, 1],
+	};
+
+	const numbers = observed.split('');
+	const adjacentsGroups = numbers.map(number => {
+		const coords = keypadCoords[number];		
+		const results = [];
+		
+		coords.forEach((coord, i) => {
+			coords[i] = coord - 1;
+			if (keypad[coords[0]]?.[coords[1]]) {
+				results.push(keypad[coords[0]][coords[1]]);
+			}
+			coords[i] = coord + 1;
+			if (keypad[coords[0]]?.[coords[1]]) {
+				results.push(keypad[coords[0]][coords[1]]);
+			}
+			coords[i] = coord;
+		});
+		results.push(parseInt(number));
+		return results;
+	});
+
+	console.log(adjacentsGroups);
+
+	for (i = 0; i < adjacentsGroups.length; i++) {
+		for (j = 0; j < adjacentsGroups[i].length; j++) {
+
+			// console.log(adjacentsGroups[i][i] + '-' + adjacentsGroups[i][j]);
 		}
 	}
-
-	for (i = 1; i < list.length; i++) {
-		if (Math.abs(list[i] - list[rangeStop]) > 1) {
-			push();
-			rangeStart = i;
-		}
-		rangeStop = i;
-	}
-
-	push();
-	return result.join(',');
+	
+	// return results;
 }
+console.log(getPINs('103'));
 
-console.log(solution([-4, -2, -1, 0, 1, 2, 3, 8, 10, 11, 12, 15, 16, 20, 21, 25]))
+// console.log(solution([-4, -2, -1, 0, 1, 2, 3, 8, 10, 11, 12, 15, 16, 20, 21, 25]))
