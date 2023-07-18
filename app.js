09957a897cc6d7488543cc846c5777f171e31de4
Will Hayman <will.hayman@aventri.com>
2023-07-19 00:50:55 +0100

    Pete the baker [https://www.codewars.com/kata/525c65e51bf619685c000059/]

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-function generateHashtag (str) {
-	if (!str || !str.match(/[A-Za-z]/)) return false;
-	let hashtag = str.replace(/^[A-Za-z]|\s+[A-Za-z]/g, match => match.toUpperCase().trim()).trim();
-	hashtag = hashtag[0] === "#" ? hashtag : "#" + hashtag;
-	return hashtag.length <= 140 ? hashtag : false;
+function cakes(recipe, available) {
+	let results = [];
+	for (const ingriedient in recipe) {
+		results.push(parseInt(available[ingriedient] / recipe[ingriedient]));
+	}
+	console.log(results)
+	return results.includes(NaN) ? 0 : Math.min(...results);
   }
 
-console.log(maxSequence('hello world'))
+
+// must return 2
+console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, milk: 200}));
+// must return 0
+// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
