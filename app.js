9951279d456a3f9ff10c43e70c3a9850075dc8d8
Will Hayman <will.hayman@aventri.com>
2023-07-19 02:12:44 +0100

    first non-repeating letter basic approach WIP

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-function cakes(recipe, available) {
-	let results = [];
-	for (const ingriedient in recipe) {
-		results.push(parseInt(available[ingriedient] / recipe[ingriedient]));
+function firstNonRepeatingLetter(s) {
+	const results = new Map;
+	for (i = 0; i < s.length; i++) {
+		results.set(s[i], results.get(s[i]) + 1 || 1)
 	}
-	console.log(results)
-	return results.includes(NaN) ? 0 : Math.min(...results);
+	for (const [key, value] of results) {
+		if (value === 1) return key;
+	}
+	return '';
   }
 
-
-// must return 2
-console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, milk: 200}));
-// must return 0
-// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}); 
+  console.log(firstNonRepeatingLetter('hellllllo'));
