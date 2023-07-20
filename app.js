1f5cf7a4b3afdb42a2f5a4986339a2e9e625ad8a
Will Hayman <will.hayman@aventri.com>
2023-07-20 22:52:20 +0100

    Strip comments [https://www.codewars.com/kata/51c8e37cee245da6b40000bd/]

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-function orderWeight(str) {
-	if (!str) return str;
-	const obj = str.split(/\s+/).map(e => {
-		return {
-			e: e,
-			sum: e.split('').reduce((acc, cur) => acc + parseInt(cur), 0)
-		}
-	});
-	obj.sort((a, b) => a['sum'] === b['sum'] ? a['e'] < b['e'] ? -1 : 1 : a['sum'] - b['sum']);
-	return obj.map(e => e['e']).join(' ');
-}
+function solution(input, markers) {
+	console.log(new RegExp("\\s*[\\" + markers.join('') + "].*", "g"));
+	return input.replace(new RegExp("\\s*[\\" + markers.join('') + "].*", "g"), "");
+};
 
-console.log(orderWeight("10 2 200 2 104"));
+// console.log(regex);
+console.log(JSON.stringify(solution("apples, plums % and bananas\npears\noranges !applesauce\noranges $applesauce", ["%", "!", "$"])));
+// console.log("apples, plums\npears\noranges");
+
+
+// Returned
+// 'apples, pears 
+// grapes
+// bananas'
+
+//but expected
+//'apples, pears
+// grapes
+// bananas'
+
+
+//"apples, plums\npears\noranges"
