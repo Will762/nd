f9451a374fd704d4c7b650157f9a3932869a01d3
Will Hayman <will.hayman@aventri.com>
2023-07-27 01:25:43 +0100

    dice score [https://www.codewars.com/kata/5270d0d18625160ada0000e4]

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-function scramble(str1, str2) {
-	if (str1.length < str2.length) return false;
-	const letters = new Map;
-	for (const char of str1) {
-		letters.set(char, letters.get(char) + 1 || 1);
+function score(diceRolls) {
+	const map = new Map;
+	let score = 0;
+	for (const roll of diceRolls) {
+		map.set(roll, map.get(roll) + 1 || 1);
 	}
-	for (const char of str2) {
-		if (letters.get(char) === 0 || letters.get(char) === undefined) return false;
-		letters.set(char, letters.get(char) - 1);
+	for (const result of map) {
+		const diceFace = result[0];
+		const diceQty = result[1];
+		const multipleOfThree = parseInt(diceQty / 3);
+		if (multipleOfThree) {
+			score += diceFace === 1 ? diceFace * multipleOfThree * 1000 : diceFace * multipleOfThree * 100;
+		}
+		if (diceFace === 1) {
+			score += diceQty % 3 * 100;
+		}
+		if (diceFace === 5) {
+			score += diceQty % 3 * 50;
+		}
 	}
-	return true;
+	return score;
   }
-  console.log(scramble('avjhh', 'java'));
+  console.log(score([1, 1, 1, 4, 4]));
